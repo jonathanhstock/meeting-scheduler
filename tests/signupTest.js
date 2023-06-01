d08c9babb51753c84cdf08615f6b83a288df5161
jonathanhstock <jonathanhstock@gmail.com>
2023-06-01 09:48:29 -0700

    edited signup test

--- tests/signupTest.js
+++ tests/signupTest.js
@@ -1,29 +1,41 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Signup from "./Signup";
-import assert from "assert";
 
-// 3 unit tests for user signup
 describe("Signup component", () => {
-  test("should handle email submission", () => {
+  test("should display error for empty email", () => {
     render(<Signup />);
 
     const emailInput = screen.getByLabelText("Email Address");
     const form = screen.getByRole("form");
 
-    // Test valid email submission
-    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
-    fireEvent.submit(form);
-    expect(screen.queryByText("Invalid email")).toBeNull();
-
-    // Test empty email submission
     fireEvent.change(emailInput, { target: { value: "" } });
     fireEvent.submit(form);
     expect(screen.getByText("Email is required")).toBeInTheDocument();
+    // Add additional assertions here to check if the error for empty email is displayed
+  });
 
-    // Test invalid email submission
+  test("should display error for invalid email", () => {
+    render(<Signup />);
+
+    const emailInput = screen.getByLabelText("Email Address");
+    const form = screen.getByRole("form");
+
     fireEvent.change(emailInput, { target: { value: "invalidemail" } });
     fireEvent.submit(form);
     expect(screen.getByText("Invalid email")).toBeInTheDocument();
+    // Add additional assertions here to check if the error for invalid email is displayed
+  });
+
+  test("should handle valid email submission", () => {
+    render(<Signup />);
+
+    const emailInput = screen.getByLabelText("Email Address");
+    const form = screen.getByRole("form");
+
+    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+    fireEvent.submit(form);
+    expect(screen.queryByText("Invalid email")).toBeNull();
+    // Add additional assertions here to check if the email was successfully submitted
   });
 });
